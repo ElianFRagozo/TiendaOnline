bc53b8ab46334fc2805af284a72f69b37f227e08
G <gonzalocal127@gmail.com>
2024-07-03 14:59:49 -0300

    Usuarios testeado, falta productos

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
 // app.js
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv');
+
+// Cargar variables de entorno
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Utiliza el puerto de las variables de entorno o el puerto 3000 por defecto
+
+// Importar rutas
 const userRoutes = require('../routes/user/userRouter');
 const productRoutes = require('../routes/product/productRouter');
-// Importar la ruta de movies
 
+// Middleware para parsear JSON
+app.use(express.json());
 
-
-app.use(express.json()); // Middleware para parsear JSON
-
+// Usar rutas
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 
-
 // Middleware para servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // Iniciar el servidor
 app.listen(port, () => {
