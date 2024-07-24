a84f6c5ce76f0bbe26e263c6a07ab77c8e729e28
ElianFRagozo <eenriquefragozo@unicesar.edu.co>
2024-07-24 09:39:43 -0500

    v2.0

---
+++ ApiUsuarios/src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Cargar variables de entorno
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000; // Utiliza el puerto de las variables de entorno o el puerto 3000 por defecto
+
+// Importar Sequelize y el modelo de usuario
+const sequelize = require('../db/db'); // Asegúrate de que la ruta sea correcta
+const User = require('../models/userModel');
+
+// Importar rutas
+const userRoutes = require('../routes/userRouter'); // Asegúrate de que la ruta sea correcta
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Usar rutas
+app.use('/user', userRoutes);
+
+// Middleware para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Sincronizar la base de datos y arrancar el servidor
+sequelize.sync({ force: false }) // Cambia force a true para forzar la sincronización (esto borrará los datos existentes)
+  .then(() => {
+    console.log('Database & tables created!');
+    app.listen(port, () => {
+      console.log(`Servidor corriendo en http://localhost:${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
